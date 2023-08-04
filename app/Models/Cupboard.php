a1ede25097142be603777043226102d209a3a954
Vyash Bhawan <vyashbhawan@gmail.com>
2023-08-05 01:56:45 +0200

    added relations to models

--- app/Models/Cupboard.php
+++ app/Models/Cupboard.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Cupboard extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cupboardItem(): HasMany
+    {
+        return $this->hasMany(CupboardItem::class);
+    }
 }
--- app/Models/CupboardItem.php
+++ app/Models/CupboardItem.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CupboardItem extends Model
 {
     use HasFactory;
+
+    public function cupboardItemType(): HasOne
+    {
+        return $this->hasOne(CupboardItemType::class);
+    }
+    
+    public function quantityType(): HasOne
+    {
+        return $this->hasOne(QuantityType::class);
+    }
+
+    public function volumeType(): HasOne
+    {
+        return $this->hasOne(VolumeType::class);
+    }
 }

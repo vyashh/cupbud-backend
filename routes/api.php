42bb613e3513e8b64f0d0fc8fd6bc6579bd28677
Vyash <67195106+vyashh@users.noreply.github.com>
2023-08-13 17:01:11 +0200

    added todo list

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// todo: create default routes
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
